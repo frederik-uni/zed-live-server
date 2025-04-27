e89523cb4ceba25ec389ccbfdd93b3cdfa7f36e2
frederik-uni <147479464+frederik-uni@users.noreply.github.com>
2025-04-27 13:26:50 +0200

    change to musl

--- src/lib.rs
+++ src/lib.rs
@@ -40,7 +40,7 @@ impl LiveServerExtension {
             },
             os = match platform {
                 zed::Os::Mac => "apple-darwin",
-                zed::Os::Linux => "unknown-linux-gnu",
+                zed::Os::Linux => "unknown-linux-musl",
                 zed::Os::Windows => "pc-windows-msvc.exe",
             },
         );
